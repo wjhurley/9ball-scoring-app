6e87c38e2cc2bd4c93cab4f6075e37f794cc5399
Will Hurley <wjhurley@users.noreply.github.com>
2017-07-02 23:08:37 -0700

    really adds the eslintrc and empty component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './9ball.png';
+import logo from './img/9ball.png';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to our 9-Ball Scoring App</h2>
+          <h2>9-Ball Scoring App</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
