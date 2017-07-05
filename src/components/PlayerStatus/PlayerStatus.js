b8e32d948f1981dd46d9e8d50231d983e476efac
Will Hurley <wjhurley@users.noreply.github.com>
2017-07-04 22:58:18 -0700

    adds Player component to store individual player info

--- src/components/PlayerStatus/PlayerStatus.js
+++ src/components/PlayerStatus/PlayerStatus.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
+import Player from './Player';
+
 import './PlayerStatus.css';
 
 class PlayerStatus extends Component {
   render() {
+    const currentPts = this.props.currentPts ? this.props.currentPts : 0,
+          requiredPts = this.props.requiredPts ? this.props.requiredPts : 0,
+          defense = this.props.defense ? this.props.defense : 0;
     return (
       <div>
-        <p>Player 1</p>
-        <p>Player 2</p>
+        <Player
+          player="Player 1"
+          currentPts={currentPts}
+          requiredPts={requiredPts}
+          defense={defense}
+        />
+        <Player
+          player="Player 2"
+          currentPts={currentPts}
+          requiredPts={requiredPts}
+          defense={defense}
+        />
       </div>
     );
   }
