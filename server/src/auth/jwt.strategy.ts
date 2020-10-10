3aa7643c7d24e48205ec2d97938e9334e8d4b2f1
wjhurley <wjhurley@comcast.net>
2020-10-10 04:12:13 -0700

    Use AppConfigService instead of env variable in jwt.strategy.ts

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,11 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+import { AppConfigService } from '../config/app/config.service';
+import { JwtConfigService } from '../config/jwt-config.service';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+
+const configService = new ConfigService();
+const appConfigService = new AppConfigService(configService);
+const jwtConfigService = new JwtConfigService(appConfigService);
+const { secret } = jwtConfigService.getJwtModuleOptions();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -15,7 +23,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: secret,
     });
   }
 
