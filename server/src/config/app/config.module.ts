d50beff0f50d1da9a7f3cc33ce945437c5257ab2
wjhurley <wjhurley@comcast.net>
2020-10-10 04:27:04 -0700

    Fix get-user.decorator.ts to prevent error in getPlayers()

--- server/src/config/app/config.module.ts
+++ server/src/config/app/config.module.ts
@@ -13,12 +13,20 @@ import configuration from './configuration';
       isGlobal: true,
       load: [configuration],
       validationSchema: Joi.object({
+        DB_DATABASE: Joi.string().default('break_n_score'),
+        DB_HOST: Joi.string().default('localhost'),
+        DB_PASS: Joi.string().default('sample_password'),
+        DB_PORT: Joi.number().default(5432),
+        DB_SYNCHRONIZE: Joi.boolean().default(true),
+        DB_TYPE: Joi.string().default('postgres'),
+        DB_USER: Joi.string().default('sample_user'),
+        JWT_EXPIRES: Joi.number().default(3600),
         JWT_SECRET: Joi.string().default('some_secret'),
         NODE_ENV: Joi.string()
           .valid('development', 'production', 'provision', 'test')
           .default('development'),
-        POSTGRES_PASS: Joi.string().default('sample_password'),
-        POSTGRES_USER: Joi.string().default('sample_user'),
+        SERVER_ORIGIN: Joi.string(),
+        SERVER_PORT: Joi.number().default(5000),
       }),
     }),
   ],
