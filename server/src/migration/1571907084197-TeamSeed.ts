a527b01b7cae0dd644b687283c686ac49f31ef68
wjhurley <wjhurley@comcast.net>
2021-02-28 20:45:41 -0800

    Update TeamSeed to query for division/hostLocation before inserting new rows

--- server/src/migration/1571907084197-TeamSeed.ts
+++ server/src/migration/1571907084197-TeamSeed.ts
@@ -1,26 +1,49 @@
 import { Logger } from '@nestjs/common';
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
 
+import { Division } from '../division/division.entity';
+import { HostLocation } from '../host-location/host-location.entity';
 import { TeamSeed } from '../seed/team.seed';
 
-export class TeamSeed1571907084194 implements MigrationInterface {
+export class TeamSeed1571907084197 implements MigrationInterface {
   private logger = new Logger('TeamSeed');
 
-  public async up(queryRunner: QueryRunner): Promise<any> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
     this.logger.log('Starting Team seed...');
 
     try {
-      const teams = await getRepository('Team').save(TeamSeed);
+      let count = 0;
 
-      if (teams) {
-        this.logger.log(`Team seed completed successfully, ${teams.length} rows added.`);
+      for (const team of TeamSeed) {
+        const division = await getRepository<Division>('Division').findOneOrFail(undefined, {
+          where: { id: team.division },
+        });
+        const hostLocation = await getRepository<HostLocation>('HostLocation').findOneOrFail(
+          undefined,
+          {
+            where: { id: team.hostLocation },
+          },
+        );
+        const teams = await getRepository('Team').save({
+          ...team,
+          division,
+          hostLocation,
+        });
+
+        if (teams) {
+          count++;
+        }
+      }
+
+      if (count > 0) {
+        this.logger.log(`Team seed completed successfully, ${count} rows added.`);
       }
     } catch (error) {
       this.logger.error('Team seed failed', error.stack);
     }
   }
 
-  public async down(queryRunner: QueryRunner): Promise<any> {
+  public async down(queryRunner: QueryRunner): Promise<void> {
     this.logger.log('Starting Team seed reversion...');
 
     try {
